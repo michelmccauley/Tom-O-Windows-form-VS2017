6d67da10d8bf0357a786d609d38111db7a9c56a8
Michel McCauley <michel.mccauley@live.com>
2018-07-24 22:42:32 -0400

    Initial Commit

---
+++ Lesson 19/Lesson 19/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson_19
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                string input = textBox1.Text;
+                double textCapture = double.Parse(input);
+                string output = $"{Math.Round(Math.Pow(textCapture, 2), 3)}";
+                MessageBox.Show(output, "Output", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message, "Please Note: " , MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+            
+
+        }
+    }
+}
---
+++ Lesson 20/Lesson 20/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson_20
+{
+    public partial class Form1 : Form
+    {
+        Label l = new Label();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Label l2 = new Label();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            l.AllowDrop = true;
+
+        }
+    }
+}
